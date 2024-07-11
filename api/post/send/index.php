80ff4974c9a0ed00135a4b046a5e32e5a8d4f1dc
Michael Monetized <michaelmonetized@gmail.com>
2024-07-11 17:06:12 -0400

    formatting and send api improvements

--- api/post/send/index.php
+++ api/post/send/index.php
@@ -8,7 +8,6 @@ if (isset($data)) {
     'headers' => [
       'From: success@hustlelaunch.com',
       'Content-Type: text/html; charset=UTF-8',
-      "Reply-To: " . $data->data->email || "success@hustlelaunch.com",
     ],
   ];
 
@@ -16,7 +15,6 @@ if (isset($data)) {
     $mail['body'] .= $key . ": " . $value . "\n";
   }
 
-  $mail['body'] .= "\n\n" . $data->ip;
   $mail['body'] .= "\n\n" . $data->pathname;
 
   mail($mail['to'], $mail['subject'], $mail['body'], $mail['headers']);
